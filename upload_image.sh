d5bbf74d6908a2066d4e94e82280a4e6d33c71de
João Pereira <83281870+JMBP7575@users.noreply.github.com>
2022-11-22 16:28:09 +0000

    fix upload image

--- upload_image.sh
+++ upload_image.sh
@@ -7,9 +7,11 @@ IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbaw2223/lbaw22114 # Replace with your group's
 
 # Ensure that dependencies are available
 composer install
+php artisan config:clear
 php artisan clear-compiled
 php artisan optimize
 
-#docker buildx build --push --platform linux/amd64 -t $IMAGE_NAME .
+# docker buildx build --push --platform linux/amd64 -t $IMAGE_NAME .
 docker build -t $IMAGE_NAME .
 docker push $IMAGE_NAME
+
