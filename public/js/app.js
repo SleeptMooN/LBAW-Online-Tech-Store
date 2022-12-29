2a022b8f08d710ba4fefa0473b3fd667239b4c6b
João Pereira <83281870+JMBP7575@users.noreply.github.com>
2022-12-29 15:31:49 +0000

    try ajax

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,4 @@
-function addEventListeners() {
-  let itemCheckers = document.querySelectorAll('article.card li.item input[type=checkbox]');
-  [].forEach.call(itemCheckers, function(checker) {
-    checker.addEventListener('change', sendItemUpdateRequest);
-  });
 
-  let itemCreators = document.querySelectorAll('article.card form.new_item');
-  [].forEach.call(itemCreators, function(creator) {
-    creator.addEventListener('submit', sendCreateItemRequest);
-  });
-
-  let itemDeleters = document.querySelectorAll('article.card li a.delete');
-  [].forEach.call(itemDeleters, function(deleter) {
-    deleter.addEventListener('click', sendDeleteItemRequest);
-  });
-
-  let cardDeleters = document.querySelectorAll('article.card header a.delete');
-  [].forEach.call(cardDeleters, function(deleter) {
-    deleter.addEventListener('click', sendDeleteCardRequest);
-  });
-
-  let cardCreator = document.querySelector('article.card form.new_card');
-  if (cardCreator != null)
-    cardCreator.addEventListener('submit', sendCreateCardRequest);
-}
 
 function encodeForAjax(data) {
   if (data == null) return null;
@@ -41,208 +17,61 @@ function sendAjaxRequest(method, url, data, handler) {
   request.send(encodeForAjax(data));
 }
 
-function sendItemUpdateRequest() {
-  let item = this.closest('li.item');
-  let id = item.getAttribute('data-id');
-  let checked = item.querySelector('input[type=checkbox]').checked;
 
-  sendAjaxRequest('post', '/api/item/' + id, {done: checked}, itemUpdatedHandler);
-}
+document.querySelectorAll('.addToCartBtn').forEach(function(element) {
+  element.addEventListener('click', function(e) {
+    e.preventDefault();
 
-function sendDeleteItemRequest() {
-  let id = this.closest('li.item').getAttribute('data-id');
+    var product_data = this.closest('.product_data');
+    var product_id = product_data.querySelector('.prod_id').value;
+    var quantity = product_data.querySelector('.input-qty').value;
 
-  sendAjaxRequest('delete', '/api/item/' + id, null, itemDeletedHandler);
-}
+    var data = {
+      product_id: product_id,
+      quantity: quantity,
+    };
 
-function sendCreateItemRequest(event) {
-  let id = this.closest('article').getAttribute('data-id');
-  let description = this.querySelector('input[name=description]').value;
-
-  if (description != '')
-    sendAjaxRequest('put', '/api/cards/' + id, {description: description}, itemAddedHandler);
-
-  event.preventDefault();
-}
-
-function sendDeleteCardRequest(event) {
-  let id = this.closest('article').getAttribute('data-id');
-
-  sendAjaxRequest('delete', '/api/cards/' + id, null, cardDeletedHandler);
-}
-
-function sendCreateCardRequest(event) {
-  let name = this.querySelector('input[name=name]').value;
-
-  if (name != '')
-    sendAjaxRequest('put', '/api/cards/', {name: name}, cardAddedHandler);
-
-  event.preventDefault();
-}
-
-function itemUpdatedHandler() {
-  let item = JSON.parse(this.responseText);
-  let element = document.querySelector('li.item[data-id="' + item.id + '"]');
-  let input = element.querySelector('input[type=checkbox]');
-  element.checked = item.done == "true";
-}
-
-function itemAddedHandler() {
-  if (this.status != 200) window.location = '/';
-  let item = JSON.parse(this.responseText);
-
-  // Create the new item
-  let new_item = createItem(item);
-
-  // Insert the new item
-  let card = document.querySelector('article.card[data-id="' + item.card_id + '"]');
-  let form = card.querySelector('form.new_item');
-  form.previousElementSibling.append(new_item);
-
-  // Reset the new item form
-  form.querySelector('[type=text]').value="";
-}
-
-function itemDeletedHandler() {
-  if (this.status != 200) window.location = '/';
-  let item = JSON.parse(this.responseText);
-  let element = document.querySelector('li.item[data-id="' + item.id + '"]');
-  element.remove();
-}
-
-function cardDeletedHandler() {
-  if (this.status != 200) window.location = '/';
-  let card = JSON.parse(this.responseText);
-  let article = document.querySelector('article.card[data-id="'+ card.id + '"]');
-  article.remove();
-}
-
-function cardAddedHandler() {
-  if (this.status != 200) window.location = '/';
-  let card = JSON.parse(this.responseText);
-
-  // Create the new card
-  let new_card = createCard(card);
-
-  // Reset the new card input
-  let form = document.querySelector('article.card form.new_card');
-  form.querySelector('[type=text]').value="";
-
-  // Insert the new card
-  let article = form.parentElement;
-  let section = article.parentElement;
-  section.insertBefore(new_card, article);
-
-  // Focus on adding an item to the new card
-  new_card.querySelector('[type=text]').focus();
-}
-
-function createCard(card) {
-  let new_card = document.createElement('article');
-  new_card.classList.add('card');
-  new_card.setAttribute('data-id', card.id);
-  new_card.innerHTML = `
-
-  <header>
-    <h2><a href="cards/${card.id}">${card.name}</a></h2>
-    <a href="#" class="delete">&#10761;</a>
-  </header>
-  <ul></ul>
-  <form class="new_item">
-    <input name="description" type="text">
-  </form>`;
-
-  let creator = new_card.querySelector('form.new_item');
-  creator.addEventListener('submit', sendCreateItemRequest);
-
-  let deleter = new_card.querySelector('header a.delete');
-  deleter.addEventListener('click', sendDeleteCardRequest);
-
-  return new_card;
-}
-
-function createItem(item) {
-  let new_item = document.createElement('li');
-  new_item.classList.add('item');
-  new_item.setAttribute('data-id', item.id);
-  new_item.innerHTML = `
-  <label>
-    <input type="checkbox"> <span>${item.description}</span><a href="#" class="delete">&#10761;</a>
-  </label>
-  `;
-
-  new_item.querySelector('input').addEventListener('change', sendItemUpdateRequest);
-  new_item.querySelector('a.delete').addEventListener('click', sendDeleteItemRequest);
-
-  return new_item;
-}
-
-addEventListeners();
-
-
-$(document).ready(function(){ 
-
-  document.querySelectorAll('.addToCartBtn').forEach(function(element) {
-    element.addEventListener('click', function(e) {
-      e.preventDefault();
-  
-      var product_id = this.closest('.product_data').querySelector('.prod_id').value;
-      var quantity = this.closest('.product_data').querySelector('.input-qty').value;
-  
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', '/add-to-cart');
-      xhr.setRequestHeader('Content-Type', 'application/json');
-      xhr.setRequestHeader('X-CSRF-TOKEN', document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          var response = JSON.parse(xhr.responseText);
-          alert(response.status);
-        }
-      };
-      xhr.send(JSON.stringify({
-        product_id: product_id,
-        quantity: quantity,
-      }));
+    sendAjaxRequest('POST', '/add-to-cart', data, function(response) {
+      var response = JSON.parse(response.target.responseText);
+      alert(response.status);
     });
   });
+});
   
-    document.querySelectorAll('.delete-cart-item').forEach(function(element) {
-      element.addEventListener('click', function(e) {
-        e.preventDefault();
-    
-        var product_id = this.closest('.product_data').querySelector('.prod_id').value;
-    
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/delete-cart-item');
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.setRequestHeader('X-CSRF-TOKEN', document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
-        xhr.onload = function() {
-          if (xhr.status === 200) {
-            var response = JSON.parse(xhr.responseText);
-            window.location.reload();
-            alert(response.status);
-          }
-        };
-        xhr.send(JSON.stringify({
-          product_id: product_id,
-        }));
-      });
+document.querySelectorAll('.delete-cart-item').forEach(function(element) {
+  element.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    var product_data = this.closest('.product_data');
+    var product_id = product_data.querySelector('.prod_id').value;
+
+    var data = {
+      product_id: product_id,
+    };
+
+    sendAjaxRequest('POST', '/delete-cart-item', data, function(response) {
+      var response = JSON.parse(response.target.responseText);
+      window.location.reload();
+      alert(response.status);
     });
+  });
+});
+
     
    
-      document.querySelectorAll('.add_btn').forEach(function(element) {
-        element.addEventListener('click', function(e) {
-          e.preventDefault();
-          
-          var add_value = this.closest('.product_data').querySelector('.input-qty').value;
-          var value = parseInt(add_value, 10);
-          value = isNaN(value) ? 0 : value;
-          if (value < 10) {
-            value++;
-            this.closest('.product_data').querySelector('.input-qty').value = value;
-          }
-        });
-      });
+  document.querySelectorAll('.add_btn').forEach(function(element) {
+    element.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      var add_value = this.closest('.product_data').querySelector('.input-qty').value;
+      var value = parseInt(add_value, 10);
+      value = isNaN(value) ? 0 : value;
+      if (value < 10) {
+        value++;
+        this.closest('.product_data').querySelector('.input-qty').value = value;
+      }
+    });
+  });
       
     
 
@@ -262,28 +91,25 @@ $(document).ready(function(){
   
 
 
- document.querySelectorAll('.changeQuantity').forEach(function(element) {
-  element.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    var product_id = this.closest('.product_data').querySelector('.prod_id').value;
-    var qty = this.closest('.product_data').querySelector('.input-qty').value;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/update-cart');
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.setRequestHeader('X-CSRF-TOKEN', document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
-    xhr.onload = function() {
-      if (xhr.status === 200) {
+  document.querySelectorAll('.changeQuantity').forEach(function(element) {
+    element.addEventListener('click', function(e) {
+      e.preventDefault();
+  
+      var product_data = this.closest('.product_data');
+      var product_id = product_data.querySelector('.prod_id').value;
+      var qty = product_data.querySelector('.input-qty').value;
+  
+      var data = {
+        product_id: product_id,
+        quantity: qty,
+      };
+  
+      sendAjaxRequest('POST', '/update-cart', data, function() {
         window.location.reload();
-      }
-    };
-    xhr.send(JSON.stringify({
-      product_id: product_id,
-      quantity: qty,
-    }));
+      });
+    });
   });
-});
+  
 
 
-});
+
