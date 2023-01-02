44a2949bc6d881e4bf473c90a1cc82795590fb63
João Pereira <83281870+JMBP7575@users.noreply.github.com>
2023-01-02 12:47:14 +0000

    Faq page

---
+++ resources/views/pages/faq.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container my-5">
+     <div class="d-grid gap-2 d-md-flex justify-content-md-center mb-4">
+        <h1>FAQ</h1>
+    </div>    
+    <div class="card">
+        <h3 class="card-body bg-dark text-white">
+            How do I place an order?
+        </h3>
+        <p> To place an order on our website, simply add the items you would like to purchase to your cart and proceed to checkout.</p>
+        </div>
+               
+            <hr class="hr-line">
+             
+     <div class="card">
+        <h3 class="card-body bg-dark text-white">
+            How do I track my order?
+        </h3>
+        <p> You can track your order by logging into your account on our website and viewing your order history.</p>
+    </div>
+
+            <hr class="hr-line">
+           
+     <div class="card">
+        <h3 class="card-body bg-dark text-white">
+            Can everyone use OnlyT3ch Store?
+        </h3>
+        <p> The website is open to everyone, however only 18+ can shop.</p>
+    </div>
+
+    <hr class="hr-line">
+           
+           <div class="card">
+              <h3 class="card-body bg-dark text-white">
+              Do you have a physical storefront that I can visit?
+              </h3>
+              <p> Now, our store is only available online</p>
+          </div>
+</div>
+
+@endsection    
