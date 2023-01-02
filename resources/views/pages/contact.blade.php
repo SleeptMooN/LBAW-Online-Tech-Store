02ad97122bc926102effe7549e46900384f30ea0
João Pereira <83281870+JMBP7575@users.noreply.github.com>
2023-01-02 11:46:52 +0000

    contact up page

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('content')
+  <div class="container my-5">
+  <div class="d-grid gap-2 d-md-flex justify-content-md-center mb-4">
+            <h1>Contacts</h1>
+        </div>
+
+
+    <div class="row pb-3">
+
+      <div class="col-md-8">
+    
+        <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=41.177967,-8.5960284&amp;t=m&amp;z=15&amp;output=embed"></iframe>
+      </div>
+    
+      <div class="col-md-4">
+        <h3>Contact details</h3>
+        <p>
+          Faculdade de Engenharia (FEUP)<br>Rua Dr. Roberto Frias<br>4200-465 PORTO<br>
+        </p>
+
+          <abbr title="Phone"></abbr> Phone: 9333333</p>
+       
+          <abbr title="Email"></abbr>Email: onlyt3ch@gmail.com
+        </p>
+        
+          <abbr title="Hours"></abbr> Monday - Sunday: 24h</p>
+        
+      </div>
+    </div>
+  </div>
+@endsection
