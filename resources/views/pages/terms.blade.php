0e1441e1fe061bcb9f60f5fac342209986056665
João Pereira <83281870+JMBP7575@users.noreply.github.com>
2023-01-02 02:36:22 +0000

    terms and conditions

---
+++ resources/views/pages/terms.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container mt-4">
+        <div class="d-grid gap-2 d-md-flex justify-content-md-center mb-4">
+            <h1>Terms and conditions</h1>
+        </div>
+        <h3>    Welcome to our online technology store! </h3>
+
+        <h5>  These terms and conditions are an agreement between the operators of OnlyT3ch and the customers.
+        By using the Website or its Services, you agreed to be bound by these Terms. If you do not agree to these Terms, please do not use the Website.
+        </h5>
+
+        <div class="row  mt-4 mb-4" ></div>
+        <h2>Accounts and Membership</h2>
+
+        <p>If you create an account on the Website, you are responsible for maintaining the security of your account and you are 
+            fully responsible for all activities that occur under the account and any other actions taken in connection with it. 
+            Providing false contact information of any kind may result in the termination of your account. </p>
+
+        <h2>Purchases</h2>
+
+        <p>All purchases made through the Website are subject to product availability. We reserve the right to refuse any order p
+            laced through the Website. We may, in our sole discretion, limit or cancel quantities purchased per person, per household or per order. </p>
+
+        <h2>Termination</h2>
+
+        <p>We reserve the right to terminate your use of the Website and its Services
+            if we believe that you have violated or acted inconsistently with the letter or spirit of these Terms. </p>
+</div>
+
+@endsection
